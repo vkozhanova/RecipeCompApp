8412961e3c2cc2e022bee715c94b96914eed7604
vkozhanova <kozhanova_vera@mail.ru>
2025-12-27 18:55:15 +0500

    RCA-08: Создать и настроить базовую навигацию между экранами

--- app/src/main/java/com/example/recipecompapp/ui/navigation/BottomNavigation.kt
+++ app/src/main/java/com/example/recipecompapp/ui/navigation/BottomNavigation.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
@@ -32,6 +33,7 @@ fun BottomNavigation(
     Row(
         modifier = Modifier
             .fillMaxWidth()
+            .navigationBarsPadding()
             .height(52.dp)
             .padding(horizontal = 16.dp, vertical = 8.dp),
 
