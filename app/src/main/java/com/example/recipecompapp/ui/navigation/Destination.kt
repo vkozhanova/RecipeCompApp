14fa77f01b7ec96d66c57ae29a5168b0fd4646d3
vkozhanova <kozhanova_vera@mail.ru>
2026-03-07 21:17:09 +0500

    RCA-17: Передавать Parcelable-объекты между экранам

--- app/src/main/java/com/example/recipecompapp/ui/navigation/Destination.kt
+++ app/src/main/java/com/example/recipecompapp/ui/navigation/Destination.kt
@@ -5,5 +5,12 @@ sealed class Destination(val route: String) {
     object Recipes : Destination("recipes/{categoryId}") {
         fun createRoute(categoryId: Int) = "recipes/$categoryId"
     }
+
     object Favorites : Destination("favorites")
+
+    object RecipeDetails : Destination("recipe/{recipeId}") {
+        fun createRoute(recipeId: Int) = "recipe/$recipeId"
+    }
 }
+
+const val KEY_RECIPE_OBJECT = "recipe_object"
