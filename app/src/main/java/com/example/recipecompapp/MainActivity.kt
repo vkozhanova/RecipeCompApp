3df45b5e1149366cfce0fc810d11199d920df695
vkozhanova <kozhanova_vera@mail.ru>
2026-05-16 22:40:15 +0500

    RCA-33: OkHttp — современный HTTP клиент

--- app/src/main/java/com/example/recipecompapp/MainActivity.kt
+++ app/src/main/java/com/example/recipecompapp/MainActivity.kt
@@ -12,13 +12,19 @@ import androidx.core.view.WindowCompat
 import com.example.recipecompapp.data.model.CategoryDto
 import com.example.recipecompapp.data.model.RecipeDto
 import kotlinx.serialization.json.Json
-import java.net.HttpURLConnection
-import java.net.URL
+import okhttp3.OkHttpClient
+import okhttp3.Request
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
+import java.util.concurrent.TimeUnit
+import kotlin.concurrent.thread
 
 class MainActivity : ComponentActivity() {
     private var deepLinkIntent by mutableStateOf(null as Intent?)
+    private val okHttpClient = OkHttpClient.Builder()
+        .connectTimeout(5, TimeUnit.SECONDS)
+        .readTimeout(5, TimeUnit.SECONDS)
+        .build()
     private lateinit var threadPool: ExecutorService
     private val jsonParser = Json { ignoreUnknownKeys = true }
 
@@ -29,26 +35,30 @@ class MainActivity : ComponentActivity() {
 
         intent?.data?.let { _ -> deepLinkIntent = intent }
 
-        threadPool.execute {
-            val threadName = Thread.currentThread().name
-            Log.i("!!!", "Выполняю запрос на потоке: $threadName")
+        thread {
+            loadCategories()
+        }
+        setContent {
+            RecipesApp(deepLinkIntent = deepLinkIntent)
+        }
+    }
 
-            var connection: HttpURLConnection? = null
-            try {
-                val url = URL("https://recipes.androidsprint.ru/api/category")
-                connection = url.openConnection() as HttpURLConnection
-                connection.connectTimeout = 3000
-                connection.readTimeout = 3000
+    private fun loadCategories() {
+        val threadName = Thread.currentThread().name
+        val request = Request.Builder()
+            .url("https://recipes.androidsprint.ru/api/category")
+            .build()
 
-                val responseCode = connection.responseCode
-                if (responseCode == HttpURLConnection.HTTP_OK) {
-                    val responseText = connection.inputStream.bufferedReader().use { it.readText() }
-                    val categories = jsonParser.decodeFromString<List<CategoryDto>>(responseText)
-
+        try {
+            okHttpClient.newCall(request).execute().use { response ->
+                val body = response.body?.string()
+                if (response.isSuccessful && body != null) {
+                    val categories = jsonParser.decodeFromString<List<CategoryDto>>(body)
                     Log.i(
                         "!!!",
                         "Количество полученных категорий: ${categories.size} на потоке $threadName"
                     )
+                    Log.i("!!!", "Body: $body")
 
                     categories.forEach { category ->
                         threadPool.execute {
@@ -56,50 +66,37 @@ class MainActivity : ComponentActivity() {
                         }
                     }
                 } else {
-                    val errorText = connection.errorStream?.bufferedReader()?.use { it.readText() }
-                    Log.e("!!!", "Ошибка загрузки категорий. Код: $responseCode, тело: $errorText")
+                    Log.e("!!!", "Ошибка загрузки категорий, код: ${response.code}, тело:$body")
                 }
-            } catch (e: Exception) {
-                Log.e("!!!", "Исключение при запросе категорий: ${e.message}", e)
-            } finally {
-                connection?.disconnect()
             }
-        }
-
-        setContent {
-            RecipesApp(deepLinkIntent = deepLinkIntent)
+        } catch (e: Exception) {
+            Log.e("!!!", "Исключение при загурзке категорий, ${e.message}", e)
         }
     }
 
     private fun loadRecipesForCategory(category: CategoryDto) {
         val threadName = Thread.currentThread().name
-        Log.i(
-            "!!!",
-            "Запрос рецептов для категории ${category.title} (id:${category.id}) на потоке: $threadName"
-        )
-        var connection: HttpURLConnection? = null
+        val request = Request.Builder()
+            .url("https://recipes.androidsprint.ru/api/category/${category.id}/recipes")
+            .build()
 
         try {
-            val url = URL("https://recipes.androidsprint.ru/api/category/${category.id}/recipes")
-            connection = url.openConnection() as HttpURLConnection
-            connection.connectTimeout = 3000
-            connection.readTimeout = 3000
+            okHttpClient.newCall(request).execute().use { response ->
+                val body = response.body?.string()
 
-            val responseCode = connection.responseCode
-            if (responseCode == HttpURLConnection.HTTP_OK) {
-                val responseText = connection.inputStream.bufferedReader().use { it.readText() }
-                val recipes = jsonParser.decodeFromString<List<RecipeDto>>(responseText)
-
-                Log.i(
-                    "!!!",
-                    "Категория ${category.title}: получено рецептов: ${recipes.size} на потоке $threadName"
-                )
-            } else {
-                val errorText = connection.errorStream?.bufferedReader()?.use { it.readText() }
-                Log.e(
-                    "!!!",
-                    "Ошибка при загрузке  рецептов для ${category.title}. Код: $responseCode, тело: $errorText"
-                )
+                if (response.isSuccessful && body != null) {
+                    val recipes = jsonParser.decodeFromString<List<RecipeDto>>(body)
+                    Log.i(
+                        "!!!",
+                        "Категория ${category.title}: получено рецептов: ${recipes.size} на потоке $threadName"
+                    )
+                    Log.i("!!!", "Body: $body")
+                } else {
+                    Log.e(
+                        "!!!",
+                        "Ошибка при загрузке  рецептов для ${category.title}. Код: ${response.code}, тело: $body"
+                    )
+                }
             }
         } catch (e: Exception) {
             Log.e(
@@ -107,8 +104,6 @@ class MainActivity : ComponentActivity() {
                 "Исключение при запросе рецептов для категории ${category.title}: ${e.message}",
                 e
             )
-        } finally {
-            connection?.disconnect()
         }
     }
 
