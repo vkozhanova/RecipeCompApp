aa9568991a6533c28b77540f9c6f71d75a230f7b
vkozhanova <kozhanova_vera@mail.ru>
2026-06-02 20:08:39 +0500

    RCA-40: Работа с DAO (Data Access Object)

--- app/src/main/java/com/example/recipecompapp/data/database/dao/CategoryDao.kt
+++ app/src/main/java/com/example/recipecompapp/data/database/dao/CategoryDao.kt
@@ -1,6 +1,8 @@
 package com.example.recipecompapp.data.database.dao
 
 import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.example.recipecompapp.data.database.entity.CategoryEntity
 import kotlinx.coroutines.flow.Flow
@@ -9,4 +11,7 @@ import kotlinx.coroutines.flow.Flow
 interface CategoryDao {
     @Query("SELECT * FROM categories ORDER BY name")
     fun getCategories(): Flow<List<CategoryEntity>>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertCategory(categories: List<CategoryEntity>)
 }
--- app/src/main/java/com/example/recipecompapp/data/database/dao/RecipeDao.kt
+++ app/src/main/java/com/example/recipecompapp/data/database/dao/RecipeDao.kt
@@ -1,6 +1,8 @@
 package com.example.recipecompapp.data.database.dao
 
 import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.example.recipecompapp.data.database.entity.RecipeEntity
 import kotlinx.coroutines.flow.Flow
@@ -9,4 +11,16 @@ import kotlinx.coroutines.flow.Flow
 interface RecipeDao {
     @Query("SELECT * FROM recipes")
     fun getAllRecipes(): Flow<List<RecipeEntity>>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertRecipesList(recipes: List<RecipeEntity>)
+
+    @Query("SELECT * FROM recipes  WHERE category_id = :categoryId")
+    fun getRecipesList(categoryId: Int): Flow<List<RecipeEntity>>
+
+    @Query("SELECT * FROM recipes WHERE id = :recipeId")
+    fun getRecipeById(recipeId: Int): Flow<RecipeEntity?>
+
+    @Query("SELECT * FROM recipes WHERE id IN (:recipeIds)")
+    fun getRecipesByIds(recipeIds: List<Int>): Flow<List<RecipeEntity>>
 }
