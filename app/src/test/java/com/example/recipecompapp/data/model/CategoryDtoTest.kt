bce4cf3e3fcb4c610f8e1f09b79cc6f219167d02
vkozhanova <kozhanova_vera@mail.ru>
2026-07-01 15:43:47 +0500

    RCA-47: Правки по ревью

--- app/src/test/java/com/example/recipecompapp/data/model/CategoryDtoTest.kt
+++ app/src/test/java/com/example/recipecompapp/data/model/CategoryDtoTest.kt
@@ -1,5 +1,6 @@
 package com.example.recipecompapp.data.model
 
+import com.example.recipecompapp.core.Constants.IMAGES_BASE_URL
 import com.example.recipecompapp.features.categories.presentation.model.toUiModel
 import org.junit.Assert.assertEquals
 import org.junit.Test
@@ -18,5 +19,6 @@ class CategoryDtoTest {
 
         assertEquals("Рыба", result.title)
         assertEquals("Блюда из рыбы", result.description)
+        assertEquals(IMAGES_BASE_URL + "fish.jpg", result.imageUrl)
     }
 }
